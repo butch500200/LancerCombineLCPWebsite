f18c156bee4305bec1cc3bf3ddc0c7ab12d6dbfc
joeyb <jbcassid@mtu.edu>
2024-02-07 16:15:22 -0500

    config for github pages

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 // vite.config.js
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    plugins: [react()],
-    esbuild: {
-        loader: 'jsx', // set loader to 'jsx' to interpret '.js' files as JSX
-        include: /\.jsx?$/, // include both .jsx and .js files
-    },
-})
+  base: '/LancerCombineLCPWebsite/',
+  plugins: [react()],
+  esbuild: {
+    loader: 'jsx', // set loader to 'jsx' to interpret '.js' files as JSX
+    include: /\.jsx?$/, // include both .jsx and .js files
+  },
+});
